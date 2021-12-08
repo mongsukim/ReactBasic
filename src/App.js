6eebc20992d58dae3d7da536d2140e8ff566e4da
mongsukim <developermong2@gmail.com>
2021-12-08 13:36:41 +0900

    Home, Movies 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import React, {useState} from 'react';
-import Movie from './components/Movie';
-import MovieForm from './components/MovieForm';
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Users from './pages/Users';
+import Movies from './pages/Movies'
+import Home from './pages/Home';
 
 function App() {
-<Router></Router>
-  const [movies, setMovies] = useState(
-    [
-     
-    ]
-  );
-
-  const removeMovie = (id) =>{
-    setMovies(movies.filter(movie=>{
-      return movie.id!==id;
-    }))
-  }
-
-  const renderMovies = movies.length ? movies.map(movie=>{
-    return(
-      <Movie 
-      movie={movie} 
-      key={movie.id}
-      removeMovie={removeMovie}
-      />
-    )
-  }) : '추가된 영화가 없습니다';
-
-  const addMovie = (movie) =>{
-    setMovies([
-      ...movies,
-      movie
-    ]);
-  }
-
   return (
     <Router>
     <div className="App">
@@ -43,15 +13,13 @@ function App() {
       <div className="container">
       <Switch>
         <Route path="/movies">
-          <h1>Movie list</h1>
-          <MovieForm addMovie={addMovie}/>
-          {renderMovies}
+          <Movies/>
         </Route>
         <Route path="/users">
           <Users/>
         </Route>
         <Route path="/" exact>
-          <h1>Home</h1>
+          <Home/>
         </Route>
       </Switch>
       </div>
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -13,7 +13,7 @@ const Users = () =>{
             setLoading(false)
         });
     },[]);
-   
+
 
     return(
         <>
